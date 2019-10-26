d1756f6aca79e7dc00937df6e3a8eaa415c82c4a
KingHarley <cweiss1995@gmail.com>
2019-10-26 19:15:13 +0800

    * Implemented 4 ColliderData algorithms * Currently have a problem with IsPointInCircle I believe

--- Source/Game/Engine/Physics/ColliderData.cpp
+++ Source/Game/Engine/Physics/ColliderData.cpp
@@ -9,25 +9,38 @@ AABBData::AABBData(Vector2 lowerBound, const Vector2 upperBound) : lowerBound(st
 
 AABBData::AABBData(Vector2 size) : AABBData(-Fixed::OneHalf * size, Fixed::OneHalf * size) {}
 
-bool AABBData::Intersecting(const AABBData&) const
+bool AABBData::Intersecting(const AABBData& other) const
 {
-	return false;
+	bool xInRange = (other.lowerBound.x >= this->lowerBound.x && other.lowerBound.x <= this->upperBound.x)
+					|| (other.upperBound.x >= this->lowerBound.x && other.upperBound.x <= this->upperBound.x);
+	bool yInRange = (other.lowerBound.y >= this->lowerBound.y && other.lowerBound.y <= this->upperBound.y)
+					|| (other.upperBound.y >= this->lowerBound.y && other.upperBound.y <= this->upperBound.y);
+	return xInRange && yInRange;
 }
 
-bool AABBData::IsPointInRect(Vector2) const
+bool AABBData::IsPointInRect(Vector2 point) const
 {
-	return false;
+	bool xInRange = (point.x >= this->lowerBound.x && point.x <= this->upperBound.x);
+	bool yInRange = (point.y >= this->lowerBound.y && point.y <= this->upperBound.y);
+	return xInRange && yInRange;
 }
 
 CircleData::CircleData(Fixed radius, Vector2 centre) : centre(std::move(centre)), radius(std::move(radius)) {}
 
-bool CircleData::IsIntersecting(const CircleData&) const
+bool CircleData::IsIntersecting(const CircleData& other) const
 {
-	return false;
+	auto length = other.radius + this->radius;
+	auto x = other.centre.x - this->centre.x;
+	auto y = other.centre.y - this->centre.y;
+
+	return (length * length) > (x * x) + (y * y);
 }
 
-bool CircleData::IsPointInCircle(Vector2) const
+bool CircleData::IsPointInCircle(Vector2 point) const
 {
-	return false;
+	auto x = point.x - this->centre.x;
+	auto y = point.y - this->centre.y;
+
+	return (this->radius * this->radius) > (x * x) + (y * y);
 }
 } // namespace ME
