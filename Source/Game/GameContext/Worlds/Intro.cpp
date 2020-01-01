34accfb926af362c69821d6fd959b3a8c0816843
Karn Kaul <karnkaul@gmail.com>
2020-01-01 15:38:44 -0800

    Add "credits" :)

--- Source/Game/GameContext/Worlds/Intro.cpp
+++ Source/Game/GameContext/Worlds/Intro.cpp
@@ -27,6 +27,30 @@ void IntroWorld::OnStarting()
 		pText->SetText(data);
 		pText->m_transform.SetPosition({0, -200});
 	}
+	{
+		TextData data;
+		data.oCharSize = 20;
+		auto hLeftBottom = NewObject<GameObject>("LeftBottom");
+		Fixed dy = 50;
+		Fixed dx = 100;
+		if (auto pLeftBottom = FindObject<GameObject>(hLeftBottom))
+		{
+			pLeftBottom->Instantiate(Primitive::Type::Text);
+			data.oText = "Karnage";
+			data.oFill = Colour(150, 10, 30);
+			pLeftBottom->SetText(data);
+			pLeftBottom->m_transform.SetPosition(g_pGFX->WorldProjection({-1, -1}) + Vector2(dx, dy));
+		}
+		auto hRightBottom = NewObject<GameObject>("RightBottom");
+		if (auto pRightBottom = FindObject<GameObject>(hRightBottom))
+		{
+			pRightBottom->Instantiate(Primitive::Type::Text);
+			data.oText = "KingHarley";
+			data.oFill = Colour(150, 20, 150);
+			pRightBottom->SetText(data);
+			pRightBottom->m_transform.SetPosition(g_pGFX->WorldProjection({1, -1}) + Vector2(-dx, dy));
+		}
+	}
 	RegisterInput([](const Input::Frame& frame) -> bool {
 		if (frame.IsReleased(KeyCode::Escape))
 		{
